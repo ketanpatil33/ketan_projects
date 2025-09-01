d3a24a052fe3e0bcea67d7ed94e04265728e8dfb
Ketan Patil <125029154+ketanpatil33@users.noreply.github.com>
2025-09-01 22:34:21 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,16 @@
+// Create a student object
+const student = {
+    name: "Ketan Patil",
+    age: 21,
+    course: "Information Technology",
+    college: "SPPU",
+    rollNo: "IT123"
+};
+
+// Print student details
+console.log("---- Student Details ----");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
+console.log("Roll Number:", student.rollNo);
